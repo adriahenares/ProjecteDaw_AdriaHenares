95645437ac1707e4999cbb4bd334ed3202d9844f
AleixViladrich <thealeixviladrich@gmail.com>
2023-12-05 18:49:26 +0100

    creacio (a falta de revisio) de tickets i intervencio

--- ProjecteDaw/app/Database/Migrations/2023-12-04-180144_TiquetsDataMigration.php
+++ ProjecteDaw/app/Database/Migrations/2023-12-04-180144_TiquetsDataMigration.php
@@ -8,11 +8,87 @@ class TiquetsDataMigration extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'deviceType'          => [
+                'type'       => 'VARCHAR',
+                'constraint' => '16',
+                null => false,
+            ],
+        ]);
+        $this->forge->addPrimaryKey('deviceType', true);
+        $this->forge->createTable('deviceType');
+
+        $this->forge->addField([
+            'status'          => [
+                'type'       => 'VARCHAR',
+                'constraint' => '16',
+                null => false,
+            ],
+        ]);
+        $this->forge->addPrimaryKey('status', true);
+        $this->forge->createTable('statusId');
+
+        $this->forge->addField([
+            'ticketId'          => [
+                'type'       => 'VARCHAR',
+                'constraint' => '16',
+                null => false
+            ],
+            'deviceType'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '16',
+            ],
+            'fault_description' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '128',
+                'null' => false,
+            ],
+            'g_center_code' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '32',
+                'null' => false,
+            ],
+            'r_center_code' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '32',
+                'null' => false,
+            ],
+            'emailOfPerson_center_g' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '64',
+                'null' => false,
+            ],
+            'nameOfPerson_center_g' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '64',
+                'null' => false,
+            ],
+            'dateOfLastModification' => [
+                'type'       => 'date',
+                'null' => false,
+            ], 
+            'registrationData' => [
+                'type'       => 'date',
+                'null' => false,
+            ], 
+            'status' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '16',
+                'null' => false,
+            ],
+           
+        ]);
+        $this->forge->addPrimaryKey('ticketid', true);
+        $this->forge->createTable('tickets');
+        $this->forge->addForeignKey('status', 'device_type', true);
     }
+
+
 
     public function down()
     {
-        //
+        $this->forge->dropTable('tickets');
+        $this->forge->dropTable('status');
+        $this->forge->dropTable('device_type');
     }
 }
--- ProjecteDaw/app/Database/Migrations/2023-12-04-180319_InterventionDataMigration.php
+++ ProjecteDaw/app/Database/Migrations/2023-12-04-180319_InterventionDataMigration.php
@@ -8,11 +8,74 @@ class InterventionDataMigration extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'interventionType'          => [
+                'type'       => 'VARCHAR',
+                'constraint' => '32',
+                null => false,
+            ],
+        ]);
+        $this->forge->addPrimaryKey('interventionType', true);
+        $this->forge->createTable('interventionType');
+
+
+        $this->forge->addField([
+            'interventionId'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '16',
+                null => false
+            ],
+            'ticketId'          => [
+                'type'       => 'VARCHAR',
+                'constraint' => '16',
+                null => false
+            ],
+            'professorId' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '16',
+                'null' => false,
+            ],
+            'studentId' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '16',
+                'null' => false,
+            ],
+            'interventionType' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '16',
+                'null' => false,
+            ],
+            'description' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '128',
+                'null' => false,
+            ],
+            'studentCourse' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '32',
+                'null' => false,
+            ],
+            'studentStudies' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '32',
+                'null' => false,
+            ], 
+            'interventionDate' => [
+                'type'       => 'date',
+                'null' => false,
+            ],  
+        ]);
+        $this->forge->addPrimaryKey('interventionId', true);
+        $this->forge->createTable('interventions');
+        $this->forge->addForeignKey('ticketId','studentId','professorId','interventionType', true);
     }
+
+
 
     public function down()
     {
-        //
+        $this->forge->dropTable('interventions');
+        $this->forge->dropTable('interventionType');
     }
 }
+
