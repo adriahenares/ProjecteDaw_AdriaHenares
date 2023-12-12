d1589c19d69e8f320ee7674e3f4f614424b6b5b9
AleixViladrich <thealeixviladrich@gmail.com>
2023-12-12 18:55:16 +0100

    creacio seeders

---
+++ ProjecteDaw/app/Database/Seeds/ProfessorSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class ProfessorSeeder extends Seeder
+{
+    public function run()
+    {
+        //
+    }
+}
---
+++ ProjecteDaw/app/Database/Seeds/StockTypeSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class StockTypeSeeder extends Seeder
+{
+    public function run()
+    {
+        //
+    }
+}
