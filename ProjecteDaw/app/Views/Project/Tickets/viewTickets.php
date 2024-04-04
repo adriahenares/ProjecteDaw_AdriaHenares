a299cb53a9209d46e8ff8f09e624f1af5504ed4c
nfaris4 <nfaris@inscaparrella.cat>
2024-04-04 19:00:58 +0200

    botó create ticket fora

--- ProjecteDaw/app/Views/Project/Tickets/viewTickets.php
+++ ProjecteDaw/app/Views/Project/Tickets/viewTickets.php
@@ -13,9 +13,6 @@
         <?= $this->include("layouts/partials/menu") ?>
         <div class="col-10">
             <?= $output ?>
-            <div>
-                <a class="m-1 w-50 btn btn-primary" href="<?php echo base_url('/addTickets') ?>">Crear Ticket</a>
-            </div>
         </div>
     </div>
 </div>
