7f75ba5921a4b99647827dac41af62fcf1b41d7d
adriahenares <71133149+adriahenares@users.noreply.github.com>
2024-04-13 09:38:21 +0200

    Migration, Seeder i Model for Logins

--- ProjecteDaw/app/Database/Migrations/2024-04-11-180019_LoginsMigration.php
+++ ProjecteDaw/app/Database/Migrations/2024-04-11-180019_LoginsMigration.php
@@ -8,11 +8,24 @@ class LoginsMigration extends Migration
 {
     public function up()
     {
-        
+        $this->forge->addField([
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '32',
+                'null' => false,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '32',
+                'null' => false,
+            ],
+        ]);
+        $this->forge->addKey('email', true);
+        $this->forge->createTable('logins');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('logins');
     }
 }
