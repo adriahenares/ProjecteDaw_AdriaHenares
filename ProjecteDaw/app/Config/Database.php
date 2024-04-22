b7f3cc9eeac4166a29251c462006a96db059847b
adriahenares <71133149+adriahenares@users.noreply.github.com>
2024-04-22 17:25:43 +0200

    change db to localhost

--- ProjecteDaw/app/Config/Database.php
+++ ProjecteDaw/app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => '85.214.28.48',
-        'username'     => 'tekplus',
-        'password'     => '?Xac9056n',
-        'database'     => 'kpatek4_tekplus',
+        'hostname'     => 'localhost',//85.214.28.48
+        'username'     => 'root',//tekplus
+        'password'     => '',//?Xac9056n
+        'database'     => 'projectedaw2023-2024',//kpatek4_tekplus
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
