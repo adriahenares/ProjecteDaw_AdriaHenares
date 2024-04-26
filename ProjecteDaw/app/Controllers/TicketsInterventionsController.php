b3087df5c98feb8890b801d3c412c5d49a8031f5
adriahenares <adriahenares@gmail.com>
2024-04-26 16:19:21 +0200

    Add where in interventions KpaCrud

--- ProjecteDaw/app/Controllers/TicketsInterventionsController.php
+++ ProjecteDaw/app/Controllers/TicketsInterventionsController.php
@@ -36,6 +36,7 @@ class TicketsInterventionsController extends BaseController
             'created_at' => ['name' => 'Data creació'],
         ]);
         // $crud->setConfig('centerView');
+        $crud->addWhere('ticket_id', $id);
         $crud->setConfig('onlyView');
         // $crud->addItemLink('del', 'fa-mail', base_url('/updateIntervention'), 'Modificar Intervencio');
         // $crud->addItemLink('view', 'fa-file', base_url('/delIntervention'), 'Eliminar Intervencio');
